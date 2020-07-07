65822b01bd87f6895da705b96f5e255ed6e3f44b
Robert Carter <racarter1215@gmail.com>
2020-07-07 15:57:26 -0700

    initial commit

--- LAB02/Program.cs
+++ LAB02/Program.cs
@@ -4,49 +4,50 @@ namespace LAB02
 {
     public class Program
     {
+        public static decimal Balance = 5000;
         static void Main(string[] args)
         {
             UserInterface();
-            Console.WriteLine("Hello World!");
         }
 
-        static void UserInterface()
+        public static void UserInterface()
         {
-            int balance = 5000;
-            while(transaction)
+            Console.WriteLine("Welcome to the ATM!");
+            int userNumber;
+            do
             {
                 Console.WriteLine("Choose your next Transaction");
-                Console.WriteLine("Deposit");
-                Console.WriteLine("Withdraw");
-                Console.WriteLine("Check your balance");
-                Console.WriteLine("Exit");
-                if (Console.ReadLine(1))
+                Console.WriteLine("1 Deposit");
+                Console.WriteLine("2 Withdraw");
+                Console.WriteLine("3 Check your balance");
+                Console.WriteLine("4 Exit");
+                userNumber = int.Parse(Console.ReadLine());
+                if (userNumber == 1)
                 {
                     Console.WriteLine("How much would you want to deposit?");
-                    string userAnswer = Console.ReadLine() + balance;
-                    int userNumber = Convert.ToInt32(userAnswer);
-                    Console.WriteLine("Are you sure?");
-                    if (y == true)
-                    {
-                        return userNumber;
-                    }
-                    else if(n == true)
-                    {
-                        UserInterface();
-                    }
+                    string userAnswer = Console.ReadLine();
+                    int depositAmount = Convert.ToInt32(userAnswer);
+                    Deposit(depositAmount);
                 }
                 else if (Console.ReadLine(2))
                 {
                     Console.WriteLine("How much would you want to withdraw?");
                     string userAnswer = Console.ReadLine();
-                    int userNumber = balance - Convert.ToInt32(userAnswer);
+                    userNumber = balance - Convert.ToInt32(userAnswer);
                     return userNumber;
                 }
                 else if (Console.ReadLine(3))
                 {
-                    Console.WriteLine($"Your current balance is {balance}")
+                    Console.WriteLine($"Your current balance is {balance}");
                 }
-            }
+
+
+            } while (userNumber != 4);
+        }
+        public static decimal Deposit(int amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
         }
     }
-}
